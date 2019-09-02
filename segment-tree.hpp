f5a53fd3812632a260efe657011fc24509d5b1c2
Tariq Ramlall <srcmake@gmail.com>
2019-09-02 11:39:41 -0400

    [st] Created allocate, deallocate, and fillin shell functions.

--- segment-tree.hpp
+++ segment-tree.hpp
@@ -12,8 +12,9 @@ namespace srcmake {
 		{
 		private:
 			T* st; // A pointer to the root of the ST.
-			// TODO: Create Empty ST
-			// TODO: Fill ST
+			void AllocateEmptyST(); // Allocate the space for an empty ST.
+			void DeallocateST(); // Delete the ST previously created.
+			void FillInST(); // Fill the ST with the proper values.
 
 		public:
 			SegmentTree(); // Constructor
@@ -28,10 +29,34 @@ namespace srcmake {
 	////////////////////////////////////////////////////////////
 	/////////// Constructor ////////////////////////////////////
 	template<class T>
+	void SegmentTree<T>::AllocateEmptyST()
+		{
+		std::cout << "Beginning allocation of memory for the segment tree.\n";
+
+		// TODO: Calculate the size of the tree and allocate an array for it.
+		// TODO: Make st point to that array.
+
+		std::cout << "Finished allocation of memory for the segment tree.\n";
+		}
+
+	template<class T>
+	void SegmentTree<T>::FillInST()
+		{
+		std::cout << "Beginning to fill in the segment tree with values.\n";
+
+		// TODO: Fill the values of the ST in.
+
+		std::cout << "Finished filling in the segment tree with values.\n";
+		}
+
+	template<class T>
 	SegmentTree<T>::SegmentTree()
 		{
 		std::cout << "Beginning construction of new segment tree.\n";
 		// TODO: Accept a vector as input and build the segment tree.
+
+		AllocateEmptyST();
+		FillInST();
 
 		std::cout << "Finished construction of segment tree.\n";
 		}
@@ -40,13 +65,23 @@ namespace srcmake {
 
 
 	////////////////////////////////////////////////////////////
-	/////////// Destructor ////////////////////////////////////
+	/////////// Destructor /////////////////////////////////////
+	template<class T>
+	void SegmentTree<T>::DeallocateST()
+		{
+		std::cout << "Beginning deallocation of memory used for the segment tree.\n";
+
+		// TODO: Delete st.
+
+		std::cout << "Finished deallocation of memory used for the segment tree.\n";
+		}
+
 	template<class T>
 	SegmentTree<T>::~SegmentTree()
 		{
 		std::cout << "Beginning destruction of segment tree.\n";
 		
-		// TODO: Deallocate the st.
+		DeallocateST();
 
 		std::cout << "Finished destruction of segment tree.\n";
 		}
